def5ac2fe511ed062fe62e3866fed70ee0c81522
jlyon87 <jml6487@gmail.com>
2016-12-20 21:01:38 -0500

    strip view1 and view2

--- app/home/home.component.js
+++ app/home/home.component.js
@@ -1,10 +1,18 @@
 'use strict';
 
 angular.
-  module('home').
-  component('home', {
+  module('home')
+
+  .config(['$routeProvider', function($routeProvider) {
+    $routeProvider.when('/home'. {
+      templateUrl : 'home/home.template.html',
+      controller : 'HomeController'
+    });
+  }])
+
+  .component('home', {
     templateUrl: 'home/home.template.html',
-    controller: ['Home',
+    controller: ['HomeController',
       function HomeController(Home) {
         this.message = 'Hello Angular!';
       }
