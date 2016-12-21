d197367aa49d5b5f7449aa463bc3ad64dbf477c9
jlyon87 <jml6487@gmail.com>
2016-12-20 21:56:18 -0500

    provider frustrations

--- app/home/home.component.js
+++ app/home/home.component.js
@@ -1,20 +1,31 @@
 'use strict';
 
-angular.
-  module('home')
+angular.module('home')
 
-  .config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/home'. {
-      templateUrl : 'home/home.template.html',
-      controller : 'HomeController'
-    });
-  }])
+  // .config(['$routeProvider', function($routeProvider) {
+  //   $routeProvider.when('/home', {
+  //     template : '<home></home>'
+  //   });
+  // }])
+
+  // .controller('HomeController', [function () {
+  //   var self = this;
+  //   self.message = 'Hello Angular!';
+  //   console.log(self.message);
+  // }])
 
   .component('home', {
-    templateUrl: 'home/home.template.html',
-    controller: ['HomeController',
-      function HomeController(Home) {
-        this.message = 'Hello Angular!';
-      }
-    ]
-  });
+    templateUrl : 'home/home.template.html',
+    controller : ['$routeParams', 'Home', HomeController]
+  })
+
+  .config(['$routeProvider', function($routeProvider) {
+    $routeProvider.when('/home', {
+      template : '<home></home>'
+    });
+  }]);
+
+function HomeController(Home) {
+  var self = this;
+  self.message = 'Hello Angular!';
+}
